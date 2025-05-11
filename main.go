727c03fbbddfe030e7fc51d9d826f5d68b4b6b60
Jakub Farnik <yakubiq@proton.me>
2025-05-11 17:19:53 +0200

    db connection

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,6 +11,8 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const ver = 2
@@ -19,7 +23,7 @@ type member struct {
 	Roles string
 	Bio   string
 	socials
-}
+} 
 type socials struct {
 	Linkedin string
 }
@@ -44,7 +48,7 @@ type language struct {
     Contakt           string
     Footer            string
 }
-
+var db *sql.DB
 
 func loadMembers() []member {
 	_members, err := os.ReadFile("data/members.json")
@@ -120,15 +124,127 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("error executing the notFound template: ", err)
 	}
 }
+
+func loadDB() string{
+	file, err := os.Open("dbpwd")
+	if err != nil{
+		fmt.Println(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan(){
+		return scanner.Text()
+	}
+	if err := scanner.Err(); err != nil{
+		fmt.Println("Error reading from dbpwd file: ", err)
+	}
+	return "" 
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request){
+	dsn := fmt.Sprintf("wtf:%s@tcp(localhost:3306)/PARADA?parseTime=true", loadDB())
+	db, err := sql.Open("mysql",dsn)
+	if err != nil{
+		log.Fatal("error opening the db: ", err)
+	}
+	if db == nil {
+		fmt.Println("DB connection is nil!")
+	} else {
+		fmt.Println("DB connection is valid.")
+	}
+	vars := mux.Vars(r)
+	sensorType := vars["type"]
+	scalarSensors := map[string]bool{
+		"temperature": true,
+		"humidity": true,
+		"pressure": true,
+	}
+	vectorSensors := map[string]bool{
+		"acceleration": true,
+		"gyroscope": true,
+		"gps": true,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if scalarSensors[sensorType]{
+		query := `
+		SELECT s.value, s.timestamp FROM sensor_data s
+		JOIN sensor_types t ON s.sensor_type_id = t.id
+		WHERE t.name = ?
+		ORDER BY s.timestamp DESC
+		LIMIT 10
+		`
+		if db == nil {
+			fmt.Println("Database connection is nil!")
+			http.Error(w, "Database not connected", http.StatusInternalServerError)
+			return
+		}
+
+		rows, err := db.Query(query, sensorType)
+		if err != nil{
+			http.Error(w, "Query error: "+err.Error(), http.StatusInternalServerError)
+			fmt.Println("query error"+err.Error())
+			return
+		}
+		defer rows.Close()
+
+		var data []map[string]interface{}
+		for rows.Next(){
+			var value float64
+			var timestamp time.Time
+			rows.Scan(&value, &timestamp)
+			data = append(data, map[string]interface{}{
+				"value": value,
+				"timestamp": timestamp.Format("15:04:05"),
+			})
+		}
+		json.NewEncoder(w).Encode(data)
+	}else if vectorSensors[sensorType]{
+		query := `
+		SELECT v.x, v.y, v.z, v.timestamp
+		FROM vector_data v
+		JOIN sensor_types t ON v.sensor_type_id = t.id
+		WHERE t.name = ?
+		ORDER BY v.timestamp DESC
+		LIMIT 10;
+		`
+		rows, err := db.Query(query, sensorType)
+		if err != nil {
+			http.Error(w, "Query error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var data []map[string]interface{}
+		for rows.Next() {
+			var x, y, z float64
+			var timestamp time.Time
+			rows.Scan(&x, &y, &z, &timestamp)
+			data = append(data, map[string]interface{}{
+				"x":         x,
+				"y":         y,
+				"z":         z,
+				"timestamp": timestamp,
+			})
+		}
+		json.NewEncoder(w).Encode(data)
+	} else{
+		fmt.Println("unknown type: ", sensorType)
+		http.Error(w, "Unknown sensor type: ", http.StatusBadRequest)
+	}
+}
+
 func main() {
 	logFile, err := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("error opening the log file: ", err)
 	}
+
 	defer logFile.Close()
 	log.SetOutput(logFile)
 
 	log.Printf("PARADA site v%d starting...", ver)
+
+
 	r := mux.NewRouter()
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("web/assets"))))
 	r.PathPrefix("/modules/").Handler(http.StripPrefix("/modules/", http.FileServer(http.Dir("web/modules"))))
@@ -136,6 +252,8 @@ func main() {
 	r.HandleFunc("/live", livePreviewHandler)
 	r.HandleFunc("/sponsors", sponsorsHandler)
 	r.NotFoundHandler = http.HandlerFunc(notFound)
+
+	r.HandleFunc("/api/data/{type}", dataHandler);
 	server := &http.Server{
 		Handler: r,
 		Addr:    ":80",
